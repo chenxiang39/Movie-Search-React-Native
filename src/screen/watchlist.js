59b0048ce24dfeb766c9d4b7a95d1dba219eee82
xiao <xiaoguo@usc.edu>
2021-04-28 19:44:12 -0700

    update

--- src/screen/watchlist.js
+++ src/screen/watchlist.js
@@ -1,27 +1,38 @@
 import * as React from 'react';
 import {useLayoutEffect, useState, useEffect,useCallback} from 'react';
-import { View, Text, StyleSheet,Dimensions,Image} from 'react-native';
+import { View, Text, StyleSheet,Dimensions,Image, ScrollView,TouchableOpacity} from 'react-native';
 import watchlistLocalStorage from '../localStorage/watchlist';
 import { useFocusEffect } from '@react-navigation/native';
-import watchlistDataModel from '../dataModel/Watchlist'
-import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+import {ContextMenuView} from "react-native-ios-context-menu"
+import Ionicons from 'react-native-vector-icons/Ionicons'
 global.deviceWidth = Dimensions.get('window').width
 global.deviceHeight = Dimensions.get('window').height
 export default  watchlist = ({navigation, route}) => {
-    const [init,Setinit] = useState(false);
+    const [loading,Setloading] = useState(false);
     const [watchlistData, SetwatchlistData] = useState([]);
+    const [isLocalData, SetisLocalData] = useState([]);
+    const [canClick, SetcanClick] = useState(true);
     useFocusEffect(
       useCallback(() => {
         fetchData();
         return () => {
-          Setinit(false);
+          Setloading(false);
         };
       }, [])
     );
+    useEffect(async ()=>{
+      await fetchData();
+      Setloading(true);
+    },[loading])
+
     const fetchData = async () =>{
       let data = await watchlistLocalStorage.loadItem();
+      await updateLocalData(data);
       SetwatchlistData(data);
-      Setinit(true);
+    }
+    const updateLocalData = async (watchlist) =>{
+      let data = await watchlistLocalStorage.checkContainItemArr(watchlist);
+      SetisLocalData(data);
     }
     useLayoutEffect(() => {
       navigation.setOptions({
@@ -29,22 +40,65 @@ export default  watchlist = ({navigation, route}) => {
       });
     }, [navigation]); 
     const itemClickFun = (item) =>{
-        navigation.navigate('Details',{
-            screen: 'detail',
-            params:{
-                id:item.id,
-                type:item.type
-            }
-        })
+        if(canClick){
+            navigation.navigate('Details',{
+              screen: 'detail',
+              params:{
+                  id:item.id,
+                  type:item.type
+              }
+          })
+        }
     }
+    const contentMenuBtnFun = async (e,item,index) =>{
+          if(!isLocalData[index]){
+              await watchlistLocalStorage.addItemToTail(item.id,item.type,item.poster_path);
+          }
+          else{
+              await watchlistLocalStorage.clearItem(item.id,item.type,item.poster_path);
+          }
+          Setloading(loading => !loading);
+      }
+                
+    const disableClick = () =>{
+        SetcanClick(false);
+    }
+    const enableClick = () =>{
+        SetcanClick(true);
+    }
+    const isInIcon = Image.resolveAssetSource(Ionicons.getImageSourceSync('bookmark',60,'black'));
+    const notInIcon = Image.resolveAssetSource(Ionicons.getImageSourceSync('bookmark-outline',60,'black'));
     const renderItem = () =>{
         let arr = [];
         for(let i = 0; i < watchlistData.length; i++){
            let cur = (
              <View style = {styles.itemContainer} key = {watchlistData[i].id + watchlistData[i].type}>
+                  <ContextMenuView
+                    lazyPreview = {false}
+                    onMenuWillShow = {()=> disableClick()}
+                    onMenuWillHide = {()=> enableClick()}
+                    // onMenuDidShow = {()=>updatelocalData(item)}
+                    onPressMenuItem={({nativeEvent})=>contentMenuBtnFun(nativeEvent,watchlistData[i],i)}
+                    previewConfig={{
+                        backgroundColor: 'white',
+                        borderRadius:0,
+                    }}
+                    menuConfig={{
+                        menuTitle: '',
+                        menuItems: [{
+                        actionKey  : 'local',
+                        actionTitle: isLocalData[i]?'Remove from watchList':'Add to watchList',
+                        icon: {
+                            iconType : 'REQUIRE',
+                            iconValue: isLocalData[i]?isInIcon:notInIcon
+                        }
+                        }],
+                    }}
+                    >
                  <TouchableOpacity onPress = {()=>itemClickFun(watchlistData[i])}>
                     <Image style ={styles.Img} source={{uri:watchlistData[i].url}}/>
                  </TouchableOpacity>
+                 </ContextMenuView>
                  <View style={styles.space}></View>
              </View>
             
@@ -76,7 +130,7 @@ export default  watchlist = ({navigation, route}) => {
     }
     return (
       <View style={styles.container}>
-          {init ? renderWatchList():(
+          {loading ? renderWatchList():(
             <View></View>
           )}
       </View>
@@ -89,12 +143,13 @@ const styles = StyleSheet.create({
     paddingLeft:10,
     paddingRight:10,
     paddingTop:120,
-    backgroundColor:'white'
+    backgroundColor:'white',
   },
   foundScrollContainer:{
     display:'flex',
     flexDirection:'row',
-    flexWrap:'wrap'
+    flexWrap:'wrap',
+    height:0.72 * deviceHeight
   },
   itemContainer:{
     display:'flex',
@@ -123,11 +178,11 @@ const styles = StyleSheet.create({
     fontWeight:'bold'
   },
   Img:{
-    height:200,
+    height:180,
     width: (deviceWidth - 20 - 6 * 2) / 3
   },
   space:{
     width:4,
-    height:200,
+    height:180,
   }
 })
